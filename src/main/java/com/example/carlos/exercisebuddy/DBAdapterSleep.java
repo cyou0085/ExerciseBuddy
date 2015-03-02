d4fea8671337afd3891296b0a5d38012aa777079
cyou0085 <cyou0085@students.vsu.edu>
2015-03-02 02:12:06 -0500

    Error with the DATABASE_CREATE query
    once i added the AM/PM to this query i received an error saying its a problem with the DisplayAllRecords in main. ALso its saying sleep is not a column in the table

--- src/main/java/com/example/carlos/exercisebuddy/DBAdapterSleep.java
+++ src/main/java/com/example/carlos/exercisebuddy/DBAdapterSleep.java
@@ -7,6 +7,10 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import com.example.carlos.exercisebuddy.Activity;
+
+import java.util.ArrayList;
+
 /**
  * Created by carlosyoung on 2/3/15.
  */
@@ -16,16 +20,18 @@ public class DBAdapterSleep {
     public static final String KEY_DAY = "day";
     public static final String KEY_SLEEP = "sleep";
     public static final String KEY_WAKE = "wake";
+    public static final String KEY_START_AM = "am1";
+    public static final String KEY_END_AM = "am2";
     //public static final String KEY_NOTES = "notes";
     private static final String TAG = "DBAdapterSleep";
 
-    private static final String DATABASE_NAME = "AssignmentsDB";
-    private static final String DATABASE_TABLE = "assignments";
-    private static final int DATABASE_VERSION = 5;
+    private static final String DATABASE_NAME = "ActivityDB";
+    private static final String DATABASE_TABLE = "activity";
+    private static final int DATABASE_VERSION = 7;
 
     private static final String DATABASE_CREATE =
             "create table if not exists " + DATABASE_TABLE + " (id integer primary key autoincrement, "
-                    + KEY_DAY + " VARCHAR not null," + KEY_ACTIVITY + " VARCHAR not null," + KEY_SLEEP + " time, " + KEY_WAKE + " time);";
+                    + KEY_DAY + " VARCHAR not null," + KEY_ACTIVITY + " VARCHAR not null," + KEY_SLEEP + "VARCHAR," + KEY_WAKE + "VARCHAR," + KEY_START_AM + "VARCHAR," + KEY_END_AM + "VARCHAR)";
 
     private final Context context;
 
@@ -53,6 +59,8 @@ public class DBAdapterSleep {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+
+
         }
 
         @Override
@@ -79,13 +87,15 @@ public class DBAdapterSleep {
     }
 
     //---insert a record into the database---
-    public long insertRecord(String activity, String day, String sleep, String awake)
+    public long insertRecord(String activity, String day, String sleep, String awake,String am1, String am2)
     {
         ContentValues initialValues = new ContentValues();
         initialValues.put(KEY_ACTIVITY,activity);
         initialValues.put(KEY_DAY,day);
         initialValues.put(KEY_SLEEP, sleep);
         initialValues.put(KEY_WAKE, awake);
+        initialValues.put(KEY_START_AM,am1);
+        initialValues.put(KEY_END_AM,am2);
         return db.insert(DATABASE_TABLE, null, initialValues);
     }
 
@@ -96,27 +106,55 @@ public class DBAdapterSleep {
     }
 
     //---retrieves all the records---
-    public Cursor getAllRecords()
+    public ArrayList<Activity> getAllRecords()
     {
-        return db.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_ACTIVITY, KEY_DAY,
-                KEY_SLEEP, KEY_WAKE}, null, null, null, null, null);
+        ArrayList<Activity> Activity_aList = new ArrayList<Activity>();
+        Cursor c = db.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_ACTIVITY, KEY_DAY,
+                KEY_SLEEP, KEY_WAKE,KEY_START_AM,KEY_END_AM}, null, null, null, null,null);
+        if(c != null){
+            if(c.moveToFirst()){
+                do{
+                    Activity Activities = new Activity();
+                    Activities.setId(c.getLong(c.getColumnIndex(DBAdapterSleep.KEY_ROWID)));
+                    Activities.setActivity(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_ACTIVITY)));
+                    Activities.setDay(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_DAY)));
+                    Activities.setStart(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_SLEEP)));
+                    Activities.setEnd(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_WAKE)));
+                    Activities.setAm1(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_START_AM)));
+                    Activities.setAm1(c.getString(c.getColumnIndex(DBAdapterSleep.KEY_END_AM)));
+                Activity_aList.add(Activities);
+                }while(c.moveToNext());
+            }
+        }
+        return Activity_aList;
     }
 
     //---retrieves a particular record---
-    public Cursor getRecord(long rowId) throws SQLException
+    public Activity getRecord(long rowId) throws SQLException
     {
+        Activity indexActivity = new Activity();
+
         Cursor mCursor =
                 db.query(true, DATABASE_TABLE, new String[] {KEY_ROWID, KEY_ACTIVITY,
-                                KEY_DAY, KEY_SLEEP, KEY_WAKE},
+                                KEY_DAY, KEY_SLEEP, KEY_WAKE,KEY_START_AM,KEY_END_AM},
                         KEY_ROWID + "=" + rowId, null, null, null, null, null);
         if (mCursor != null) {
             mCursor.moveToFirst();
         }
-        return mCursor;
+
+        indexActivity.setId(mCursor.getLong(mCursor.getColumnIndex(DBAdapterSleep.KEY_ROWID)));
+        indexActivity.setActivity(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_ACTIVITY)));
+        indexActivity.setDay(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_DAY)));
+        indexActivity.setStart(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_SLEEP)));
+        indexActivity.setEnd(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_WAKE)));
+        indexActivity.setAm1(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_START_AM)));
+        indexActivity.setAm2(mCursor.getString(mCursor.getColumnIndex(DBAdapterSleep.KEY_END_AM)));
+
+        return indexActivity;
     }
 
     //---updates a record---
-    public boolean updateRecord(int rowId, String activity,String day, String sleep, String awake)
+    public boolean updateRecord(long rowId, String activity,String day, String sleep, String awake,String am1, String am2)
     {
         ContentValues args = new ContentValues();
         args.put(KEY_ROWID,rowId);
@@ -124,6 +162,8 @@ public class DBAdapterSleep {
         args.put(KEY_DAY, day);
         args.put(KEY_SLEEP, sleep);
         args.put(KEY_WAKE, awake);
+        args.put(KEY_START_AM,am1);
+        args.put(KEY_END_AM,am2);
         return db.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
     }
 
