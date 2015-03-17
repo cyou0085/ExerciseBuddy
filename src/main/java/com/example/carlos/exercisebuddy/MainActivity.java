6131fda5de6c1967bb8a2f27ebc8a4a3798b9578
cyou0085 <cyou0085@students.vsu.edu>
2015-03-17 00:10:35 -0400

    Updated coded with the WeekView
    cleaned up some code and added commits

--- src/main/java/com/example/carlos/exercisebuddy/MainActivity.java
+++ src/main/java/com/example/carlos/exercisebuddy/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends ActionBarActivity {
     ActivityListAdapter adapter;
     ArrayList<Activity> activityList = new ArrayList <Activity>();
     public final static String ACTIVITY_ID = "com.example.carlos.exercisebuddy.MainActivity.tvID";
+
+
     //@Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -48,18 +50,15 @@ public class MainActivity extends ActionBarActivity {
             e.printStackTrace();
         }
 
-        com.example.carlos.exercisebuddy.DBAdapterSleep db = new com.example.carlos.exercisebuddy.DBAdapterSleep(this);
+        DBAdapterActivity db = new DBAdapterActivity(this);
 
-
-        // Get the message from the intent
+        // Get all the Activities from the db and puts them in an ArrayList
+        //Then Displays the List in the View
         db.open();
-        //Cursor c = db.getAllRecords();
         ArrayList <Activity>ActivityList = db.getAllRecords();
         activityList = ActivityList;
-        DisplayRecord(activityList);
+        FindRecord();
         db.close();
-
-
 
     }
 
@@ -96,6 +95,7 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    //Adds an Activity to db
     public void AddActivity(View view){
         Intent intent;
 
@@ -104,30 +104,22 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-    public void EditActivity(View view){
+    //Takes you the Week View of your DataBase
+    /*
+    public void WeekActivity(View view){
 
         Intent intent;
-        intent = new Intent(getBaseContext(), UpdateActivity.class);
+        intent = new Intent(getBaseContext(), WeekView.class);
 
         startActivity(intent);
 
-    }
+    }*/
 
 
-    public void DisplayRecord(ArrayList aList)
+    public void FindRecord()
     {
-
-        Activity activities;
         ListView lv = (ListView)findViewById(R.id.listView1);
-/*
-        if(c.moveToFirst()){
-            do{
 
-                activities = new Activity(Long.valueOf(c.getString(0)),c.getString(1),c.getString(2),c.getString(3),c.getString(4));
-                activityList.add(activities);
-            }while(c.moveToNext());
-        }
-        }*/
         adapter = new ActivityListAdapter(activityList, MainActivity.this);
         lv.setAdapter(adapter);
         lv.setClickable(true);
@@ -136,7 +128,6 @@ public class MainActivity extends ActionBarActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(getBaseContext(),UpdateActivity.class);
                 String activityId = Long.toString(activityList.get(position).getID());
-                //String activityId = activityList.get(position).getID();
                 Log.i("DW", "Adding " + activityId + " to intent");
                 i.putExtra(ACTIVITY_ID, activityId);
                 startActivity(i);
